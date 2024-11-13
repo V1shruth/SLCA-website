6baa4c18c6a8365f32ba3bb99337a655ebc9c04a
V1shruth <emailvishhere@gmail.com>
2024-11-13 18:42:47 -0500

    Intialize Repo For SLCA Website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <> 
+      <h1 className="text-3xl font-bold underline">Welcome to Student Led Chess Association!</h1>
+
+
+
+
+    </>
+  );
+};
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = ({ className, href, onClick, children, px, white}) => {
+  return (
+    <div>
+      Button
+    </div>
+  )
+}
+
+export default Button

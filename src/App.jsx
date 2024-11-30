6e98a393ad8e59698a355b0820f39560d3189b80
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-11-29 23:07:34 -0600

    Added sponsor slider, need to resize images now

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import Button from "./components/Button";
+import Sponsors from "./components/Sponsors";
 
 const App = () => {
   return (
-    <> 
-      <h1 className="text-3xl font-bold underline">Welcome to Student Led Chess Association!</h1>
-
-
-
-
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Student Led Chess Association!
+      </h1>
+      <div
+        style={{
+          maxWidth: "75vw",
+          width: "100%",
+          height: "500px",
+          margin: "0 auto",
+        }}
+      >
+        <Sponsors />
+      </div>
     </>
   );
 };
 
-export default App
+export default App;
